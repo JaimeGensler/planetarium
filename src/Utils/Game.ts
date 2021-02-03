2f46dbf22fd8f4dc0e19b9bc18c878877fca6852
Jaime Gensler <jaimegensler0@gmail.com>
2021-02-03 13:15:51 -0800

    Add basic setup

---
+++ src/Utils/Game.ts
@@ -0,0 +1,68 @@
+import * as THREE from 'three';
+
+type FrameListener = (delta: number) => void;
+
+export default class Game {
+	private readonly __sizes = {
+		height: window.innerHeight,
+		width: window.innerWidth,
+	};
+
+	public readonly scene: THREE.Scene;
+	public readonly renderer: THREE.WebGLRenderer;
+	public readonly camera: THREE.PerspectiveCamera;
+
+	private __frameListeners: FrameListener[] = [];
+	private __previousTime: number = 0;
+
+	public constructor() {
+		const canvas = document.getElementById('webgl');
+		if (!(canvas instanceof HTMLCanvasElement)) {
+			throw new Error('Could not find HTML Canvas Element with id "webgl"');
+		}
+		this.renderer = new THREE.WebGLRenderer({ canvas });
+		this.renderer.setSize(this.__sizes.width, this.__sizes.height);
+		this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+		window.addEventListener('resize', this.__handleResize.bind(this));
+
+		this.scene = new THREE.Scene();
+
+		this.camera = new THREE.PerspectiveCamera(
+			75,
+			this.__sizes.width / this.__sizes.height,
+			0.1,
+			100,
+		);
+		this.addToScene(this.camera);
+	}
+
+	public start() {
+		this.__tick(0);
+	}
+	public addFrameListener(l: FrameListener) {
+		this.__frameListeners.push(l);
+	}
+	public addToScene(...objects: THREE.Object3D[]) {
+		this.scene.add(...objects);
+	}
+
+	// ====== Private API =====
+	private __tick(frameTime: number) {
+		const delta = frameTime - this.__previousTime;
+		this.__previousTime = frameTime;
+
+		this.__frameListeners.forEach(l => l(delta));
+		this.renderer.render(this.scene, this.camera);
+		window.requestAnimationFrame(this.__tick);
+	}
+	private __handleResize() {
+		this.__sizes.height = window.innerHeight;
+		this.__sizes.width = window.innerWidth;
+
+		this.camera.aspect = this.__sizes.width / this.__sizes.height;
+		this.camera.updateProjectionMatrix();
+
+		this.renderer.setSize(this.__sizes.width, this.__sizes.height);
+		this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export default {};
+import * as THREE from 'three';
+import Game from './Utils/Game';
+
+const game = new Game();
+
+const cube = new THREE.Mesh(
+	new THREE.BoxGeometry(1, 1, 1, 1, 1, 1),
+	new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+);
+game.addToScene(cube);
+game.camera.position.z = 4;
+game.start();
