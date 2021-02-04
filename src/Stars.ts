36ee7fe764d66e9ef6f1b63ac71e025cb9cbd4e4
Jaime Gensler <jaimegensler0@gmail.com>
2021-02-04 15:24:26 -0800

    Cleanup Stars

--- src/Stars.ts
+++ src/Stars.ts
@@ -1,32 +1,19 @@
 import * as THREE from 'three';
 import Random from './utils/Random';
 
-const starCount = 3_000;
-const radiusMin = 22;
-const radiusMax = 32;
-const starSize = 0.33;
-
-type StarsParameters = { starTexture: THREE.Texture };
+type StarsParameters = { starTexture: THREE.Texture } & typeof Stars.DEFAULTS;
 export default class Stars extends THREE.Points {
-	public constructor(settings?: StarsParameters) {
+	public starCount: number;
+	public radiusMin: number;
+	public radiusMax: number;
+	public starSize: number;
+	public constructor(settings?: Partial<StarsParameters>) {
 		const starTexture = settings?.starTexture;
-		const positionHelper = new THREE.Vector3();
-
-		const positions = new Float32Array(starCount * 3);
-		for (let i = 0; i < starCount; i++) {
-			positionHelper
-				.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5)
-				.normalize()
-				.multiplyScalar(Random.floatBetween(radiusMin, radiusMax));
-			const { x, y, z } = positionHelper;
-
-			const i3 = i * 3;
-			positions[i3] = x;
-			positions[i3 + 1] = y;
-			positions[i3 + 2] = z;
-		}
+		const starCount = settings?.starCount ?? Stars.DEFAULTS.starCount;
+		const starSize = settings?.starSize ?? Stars.DEFAULTS.starSize;
 
 		const geometry = new THREE.BufferGeometry();
+		const positions = new Float32Array(starCount * 3);
 		geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
 
 		const material = new THREE.PointsMaterial({
@@ -39,6 +26,28 @@ export default class Stars extends THREE.Points {
 			transparent: !!starTexture,
 		});
 		super(geometry, material);
+
+		this.starCount = starCount;
+		this.starSize = starSize;
+		this.radiusMin = settings?.radiusMin ?? Stars.DEFAULTS.radiusMin;
+		this.radiusMax = settings?.radiusMax ?? Stars.DEFAULTS.radiusMax;
+		this.__create();
+	}
+	private __create() {
+		const positionHelper = new THREE.Vector3();
+		const positions = this.geometry.attributes.position.array as Float32Array;
+		for (let i = 0; i < this.starCount; i++) {
+			positionHelper
+				.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5)
+				.normalize()
+				.multiplyScalar(Random.floatBetween(this.radiusMin, this.radiusMax));
+			const { x, y, z } = positionHelper;
+
+			const i3 = i * 3;
+			positions[i3] = x;
+			positions[i3 + 1] = y;
+			positions[i3 + 2] = z;
+		}
 	}
 
 	public dispose() {
@@ -49,4 +58,10 @@ export default class Stars extends THREE.Points {
 			this.material.dispose();
 		}
 	}
+	public static readonly DEFAULTS = {
+		starCount: 3_000,
+		radiusMin: 20,
+		radiusMax: 40,
+		starSize: 0.33,
+	};
 }
