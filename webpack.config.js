fa3f832986cd430f7a3180ee1fd245bec36cfa07
Jaime Gensler <jaimegensler0@gmail.com>
2021-02-04 13:21:21 -0800

    Add config for image loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
@@ -16,7 +17,18 @@ module.exports = {
 				test: /\.ts$/,
 				use: 'ts-loader',
 				exclude: /node_modules/,
-			}
+			},
+			{
+				test: /\.(jpg|png|gif|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							outputPath: 'assets/images/',
+						},
+					},
+				],
+			},
 		],
 	},
 	resolve: {
@@ -27,6 +39,9 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: './src/index.html',
 			title: 'Development',
+		}),
+		new CopyWebpackPlugin({
+			patterns: [{ from: path.resolve(__dirname, './static') }],
 		}),
 	],
 	output: {
